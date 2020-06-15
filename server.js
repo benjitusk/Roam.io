43c17eadd83fe5ff835fbffe679aa5082c3e5f3a
benjixinator <benjitusk1@gmail.com>
2020-06-14 23:14:51 -0400

    Fixed ping timeout bug

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  'pingTimeout': 180000,
+  'pingInterval': 25000
+});
 server.listen(80);
-console.log(`Server listening on *:80`);
+console.log('Server listening on *:80');
 
-app.use("/", express.static("client"));
+app.use('/', express.static('client'));
 
-io.on(`connection`, (client) => {
+io.on('connection', (client) => {
   client.start = Date.now();
-  console.log(`New Client`);
-  client.emit(`welcome`, `Client connected.`);
+  console.log('New Client');
+  client.emit('welcome', 'Client connected.');
   client.on('msg', (data) => {
     console.log(data);
   });
-  client.on("disconnect", (reason) => {
+  client.on('mouseMove', (data) => {
+    console.log(data);
+  })
+  client.on('disconnect', (reason) => {
     console.log(`Client disconnected after ${(Date.now() - client.start)/1000} seconds because ${reason}.`)
   });
 });
